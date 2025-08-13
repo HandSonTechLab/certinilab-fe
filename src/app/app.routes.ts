f7d0900c1bf338d2a6191cd39625f5d70f31bd5e
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2025-08-13 16:18:50 +0200

    Add CreateSupplierComponent with basic setup and routing
    Introduce a new `CreateSupplierComponent` with corresponding HTML, CSS, and test files. Update `app.routes.ts` to include a route for creating a new supplier at the `new-supplier` path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {ClientsComponent} from './pages/clients/clients.component';
 import {SuppliersComponent} from './pages/suppliers/suppliers.component';
 import {ReportComponent} from './pages/report/report.component';
 import {SaleComponent} from './pages/sale/sale.component';
 import {LogoutComponent} from './menu/logout/logout.component';
 import {CreateClientComponent} from './pages/create-client/create-client.component';
+import {CreateSupplierComponent} from './pages/create-supplier/create-supplier.component';
 
 export const routes: Routes = [
   {
@@ -18,6 +19,10 @@ export const routes: Routes = [
   {
     path: 'suppliers', // <your-domain>/path
     component: SuppliersComponent,
+  },
+  {
+    path: 'new-supplier', // <your-domain>/path
+    component: CreateSupplierComponent,
   },
   {
     path: 'report', // <your-domain>/path
---
+++ src/app/pages/create-supplier/create-supplier.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-create-supplier',
+  imports: [],
+  templateUrl: './create-supplier.component.html',
+  styleUrl: './create-supplier.component.css'
+})
+export class CreateSupplierComponent {
+
+}
