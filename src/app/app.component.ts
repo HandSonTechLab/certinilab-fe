d47e86a109ec725a8fa8ebc1e16457dc2f40422b
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2026-02-13 00:21:55 +0100

    wip: configuring MSAL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {RouterOutlet} from '@angular/router';
 import {ErrorService} from './shared/error.service';
 import {ErrorModalComponent} from './shared/modal/error-modal/error-modal.component';
 import {CONSTANTS} from './shared/constants';
+import {MsalService} from '@azure/msal-angular';
+import {msalConfig} from './auth-config';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +19,11 @@ export class AppComponent {
   private errorService = inject(ErrorService);
   error = this.errorService.error;
   protected readonly errorTitle = CONSTANTS.create_client_request_error_title;
+  private authService = inject(MsalService);
+
+  logout() {
+    this.authService.logoutRedirect({
+      postLogoutRedirectUri: msalConfig.auth.authority
+    })
+  }
 }
